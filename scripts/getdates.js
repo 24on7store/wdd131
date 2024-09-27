5b554882f903feb3137e15e61140a05e55f56233
24on7store <132136928+24on7store@users.noreply.github.com>
2024-09-27 06:13:39 +0300

    Late assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+var currentYear = new Date().getFullYear();
+
+var copyrightParagraph = document.getElementById("copyright");
+copyrightParagraph.innerHTML = + currentYear;
+
+var lastModifiedParagraph = document.getElementById('lastModified');
+var lastModifiedDate = document.lastModified;
+lastModifiedParagraph.innerHTML = 'Last Modified' + lastModifiedDate;
+
