9b9085b59cc0793bfe8fd473c94ad2f3f3eea222
24on7store <132136928+24on7store@users.noreply.github.com>
2024-10-08 23:45:07 +0300

    week02

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,15 @@ var lastModifiedParagraph = document.getElementById('lastModified');
 var lastModifiedDate = document.lastModified;
 lastModifiedParagraph.innerHTML = 'Last Modification: ' + lastModifiedDate;
 
+
+
+var navBar = document.getElementById("navBar");
+
+function showMenu(){
+    navBar.style.right = "0";
+}
+
+function hideMenu(){
+    navBar.style.right = "-200px";
+}
+
